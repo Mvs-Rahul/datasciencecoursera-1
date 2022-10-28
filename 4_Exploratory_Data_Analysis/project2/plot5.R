d3b4ad18d83103fae8c8622140b36c273145b332
Mvs-Rahul <112823898+Mvs-Rahul@users.noreply.github.com>
2022-10-28 21:18:03 +0530

    Course Project 2

--- 4_Exploratory_Data_Analysis/project2/plot5.R
+++ 4_Exploratory_Data_Analysis/project2/plot5.R
@@ -3,20 +3,16 @@ library("ggplot2")
 
 setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project2")
 path <- getwd()
-download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
-              , destfile = paste(path, "dataFiles.zip", sep = "/"))
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", destfile = paste(path, "dataFiles.zip", sep = "/"))
 unzip(zipfile = "dataFiles.zip")
 
-# Load the NEI & SCC data frames.
 NEI <- data.table::as.data.table(x = readRDS("summarySCC_PM25.rds"))
 SCC <- data.table::as.data.table(x = readRDS("Source_Classification_Code.rds"))
 
-# Gather the subset of the NEI data which corresponds to vehicles
 vehiclesSCC <- SCC[grepl("vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
                    , SCC]
 vehiclesNEI <- NEI[NEI[, SCC] %in% vehiclesSCC,]
 
-# Subset the vehicles NEI data to Baltimore's fip
 baltimoreVehiclesNEI <- vehiclesNEI[fips=="24510",]
 
 png("plot5.png")
